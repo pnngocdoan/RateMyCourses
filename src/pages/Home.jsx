4d24037a0538aea1a10650a966471594b75824f8
Ngoc Doan <ndoan@soka.edu>
2023-04-25 01:41:32 -0700

    add sorting feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,25 +5,49 @@ import './Home.css'
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async() => {
+    const [sort, setSort] = useState("Newest");
+    const fetchData = async() => {
+        if (sort == "Newest") {
             const {data} = await supabase
-                                    .from('Posts')
-                                    .select()
-                                    .order('created_at', { ascending: true})
+                            .from('Posts')
+                            .select()
+                            .order('created_at', { ascending: false})
             setPosts(data);
         }
+        else {
+            const {data} = await supabase
+                            .from('Posts')
+                            .select()
+                            .order('voteCount', { ascending: false})  
+            setPosts(data);          
+        }
+    }
+    useEffect(() => {
         fetchData();
-    }, [posts]);
+        console.log(posts);
+    }, [sort]);
     return(
         <div className="home-page">
-            {
-            posts && posts.length > 0 ? 
-            posts.map((post, index) => 
-            <Post id={post.id} course_name={post.course_name} prof_name={post.prof_name} created_at={post.created_at} vote_count={post.voteCount} />)
-            :
-            <h2>No Ratings Yet</h2>}
+            <div className="sort-container">
+                <h4> Sorted by </h4>
+                <button className="sort-btn" onClick={() => {
+                    setSort("Newest");
+               //     fetchData();
+                }}>Newest</button>
+                <button className="sort-btn" onClick={() => {
+                    setSort("Popular");
+                //    fetchData();
+                }}>Popular</button>
+            </div>
+            <div className="posts-container">
+                {
+                posts && posts.length > 0 ? posts.map((post, index) => 
+                    <Post key={post.id} id={post.id} course_name={post.course_name} prof_name={post.prof_name} created_at={post.created_at} vote_count={post.voteCount} />)
+                :
+                    <h2>No Ratings Yet</h2>
+                }
+            </div>
+
         </div>
     )
 }
